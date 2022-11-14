509a9158854b5f2ba13bbd130a38a91f3940d333
theautomation <github@theautomation.nl>
2022-11-14 19:23:45 +0100

    Change get ip command for Alpine

--- src/shelly-init.sh
+++ src/shelly-init.sh
@@ -5,14 +5,12 @@
 # github@theautomation.nl
 #
 
-set -e
-
 DOMAIN="lan"
 IP_SUBNET="192.168.40"
 IP_START="99"
 IP_END="140"
 
-ip=$(hostname -I | cut -d' ' -f1)
+ip=$(hostname -i)
 for i in $(seq ${IP_START} ${IP_END}); do
     curl -m 2 "http://${IP_SUBNET}.${i}/settings?coiot_enable=true&coiot_peer=${ip}" 2>/dev/null | jq && curl -m 2 "http://${IP_SUBNET}.${i}/reboot" 2>/dev/null | jq
 done
