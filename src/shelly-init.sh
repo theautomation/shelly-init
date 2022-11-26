b456631e21f1882cceac3cdf95b3d5fbab6e4e82
theautomation <github@theautomation.nl>
2022-11-26 12:22:51 +0100

    Changed comment in script

--- src/shelly-init.sh
+++ src/shelly-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Put the Home Assistant host ip in CoIot field for all Shelly devices in the network.
+# This script puts the IP address of the Home Assistant host in the CoIot field for all Shelly devices on the network and reboots the device to apply it.
 # Writtin by Coen Stam.
 # github@theautomation.nl
 #
@@ -10,7 +10,7 @@ IP_SUBNET="192.168.40"
 IP_START="99"
 IP_END="140"
 
-ip=$(/sbin/ip route|tail -1|awk 'NF>1{print $NF}')
+ip=$(/sbin/ip route | tail -1 | awk 'NF>1{print $NF}')
 for i in $(seq ${IP_START} ${IP_END}); do
     curl -m 2 "http://${IP_SUBNET}.${i}/settings?coiot_enable=true&coiot_peer=${ip}" 2>/dev/null | jq && curl -m 2 "http://${IP_SUBNET}.${i}/reboot" 2>/dev/null | jq
 done
